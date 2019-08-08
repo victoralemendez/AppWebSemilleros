50968349f9b5d4be00175d3d2fbd024a9bfb09a7
victoralemendez <victoralemendez@unicauca.edu.co>
2019-08-08 15:54:51 -0500

    implementacion de configuracion

---
+++ api/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ipdb: "localhost",
+    portdb: 27017,
+    portApp: 3977
+}
--- api/index.js
+++ api/index.js
@@ -3,18 +3,20 @@
 
 var mongoose = require('mongoose');     // Modulo intermediario para trabajar con mongodb
 var app = require('./app');
-var port = process.env.PORT || 3977;
+var config = require('./config');
+var port = process.env.PORT || config.portApp;
+
 
 mongoose.Promise = global.Promise; // Elimina mensajes de mongoose al ejecutar el servidor
 
 // Conexion a la bd
-mongoose.connect('mongodb://localhost:27017/semillero', {useNewUrlParser: true},(err, res) => {
+mongoose.connect('mongodb://'+ config.ipdb + ':' + config.portdb + '/semillero', {useNewUrlParser: true},(err, res) => {
     if (err) {
         throw err;
     } else {
         console.log("la conexión con la base de datos está funcionando correctamente");
         app.listen(port, function() {
-            console.log("Servidor del API Rest de semilleros escuchando en http://localhost:" + port);
+            console.log("Servidor del API Rest de semilleros escuchando en el puerto: " + config.portApp);
         });
     }
 });
